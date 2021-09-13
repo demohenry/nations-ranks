75ee7969d77190828da1983f1084c7356a992900
Democrito <demohenry6@gmail.com>
2021-09-13 14:10:44 -0300

    CountriesTable sort

--- src/components/CountriesTable/index.tsx
+++ src/components/CountriesTable/index.tsx
@@ -1,8 +1,25 @@
 import { CountriesProps } from 'pages';
 import * as S from './styles';
 
+const orderBy = ({ countries }: CountriesProps, direction: string) => {
+	if (direction === 'asc') {
+		return [...countries].sort((a, b) =>
+			a.population > b.population ? 1 : -1
+		);
+	}
+
+	if (direction === 'desc') {
+		return [...countries].sort((a, b) =>
+			a.population > b.population ? -1 : 1
+		);
+	}
+
+	return countries;
+};
+
 const CountriesTable = ({ countries }: CountriesProps) => {
-	console.log(countries);
+	const orderedCountries = orderBy({ countries }, 'desc');
+
 	return (
 		<S.Wrapper>
 			<S.Heading>
@@ -15,7 +32,7 @@ const CountriesTable = ({ countries }: CountriesProps) => {
 				</button>
 			</S.Heading>
 
-			{countries.map((country) => (
+			{orderedCountries.map((country) => (
 				<div key={country.id} className="row">
 					<div className="name">{country.name}</div>
 
